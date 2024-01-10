4f1e366368baa3f846caa123c7654ab5a9e70a81
Robert Petre <petrerobertcristian@gmail.com>
2024-01-10 02:57:43 +0200

    Minor changes

--- src/scanner.c
+++ src/scanner.c
@@ -117,19 +117,51 @@ static bool isAlphanumeric(Scanner* scanner)
 		return ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_');
 }
 
+static bool match(char* word, int length, char* other_word, int length2)
+{
+	if(length1 != length2)
+		return false;
+
+	return strcmp(word, other_word);
+}
+
+static bool isRegister(char* word, int length)
+{
+	if(length > 3 || length < 2)
+		return false;
+	
+	if(word[0] == 'x' || word[0] == 'f')
+	{
+		if(length == 2 && isNumeric(word[1]))
+			return true;
+		else if(length == 3 && (word[1] == '1' || word[1] == '2') && isNumeric(word[2]))
+			return true;
+		else if(length == 3 && word[1] == '3' && (word[2] == '0' || word[2] == '1' || word[2] == '2'))
+			return true;
+	}
+
+	return false;
+}
+
 static TokenType identifierType(Scanner* scanner)
 {
 	int length = scanner->current - scanner->start;
-	char* word = (char*)malloc(length * sizeof(char));
+	char* word = (char*)malloc((length + 1) * sizeof(char));
+	word[length] = '\0';
 	for(int i = 0; i < length; i++)
 	{
 		word[i] = *(scanner->start + i);
 	}
 	toLowercase(&word);
+
 	if(findWord(scanner->trie, word))
 		return TOKEN_INSTRUCTION;
 
-	return TOKEN_LABEL;
+	// check if it's a registers
+	if(isRegister(word, length))
+		return TOKEN_REGISTER;
+
+	return TOKEN_LABEL; // otherwise it's a label of some kind
 }
 
 Scanner* initScanner(char* source)
